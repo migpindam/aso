a09da14cf7418f29fef998c52ad2eca80c7d89c9
migpindam <migpindam@alu.edu.gva.es>
2024-09-17 17:52:31 +0200

    Acabamos en ejercicio 7

--- t1_scripting/ej7.sh
+++ t1_scripting/ej7.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+#Defino la variable de la fecha.
 
+fecha=$(date +%Y-%m-%d)
+
+#Defino la variable nombre del archivo final... Ejemplo:
+
+nombre="$fecha$1.tar.gz"
+
+#Comprimo el directorio $1  en el archivo con nombre $nombre.
+
+touch "$nombre"
