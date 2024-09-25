64bb9b773265116a3ebcbbf6b3762f510591a997
migpindam <migpindam@alu.edu.gva.es>
2024-09-25 09:34:26 +0200

    ejercicio 20

--- t1_scripting/ej20.sh
+++ t1_scripting/ej20.sh
@@ -2,8 +2,18 @@
 
 #Crea un script primo.sh que verifique si el número pasado por parámetro es primo o no.
 
-modulo=1
-contador=1
+modulo=$(( $1 % 2 ))
+contador=3
 
-while [[ $modulo -ne 0 ]]; do
+if [[ $modulo -ne 0 && $1 -ne 1 ]]; then
+  while [[ $modulo -ne 0 ]]; do
+    modulo=$(($1%contador))
+    ((contador++))
+  done
+fi
   
+if [[ $contador -ge $1 ]]; then
+  echo "El número $1 es primo"
+else
+  echo "El número $1 no es primo"
+fi
