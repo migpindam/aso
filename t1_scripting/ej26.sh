ee4197724b8122339987cfc95a758545f9eefb9e
migpindam <migpindam@alu.edu.gva.es>
2024-09-30 13:48:38 +0200

    Reformas

--- t1_scripting/ej26.sh
+++ t1_scripting/ej26.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#Realiza un script que busque cualquier fichero que pueda ser modificado por cualquier usuario (— — rwx) y guarde la lista de ficheros con la ruta exacta en el archivo archivos_peligrosos.txt
+#Realiza un script que busque cualquier fichero que pueda ser modificado por cualquier usuario (— — rwx) y guarde la lista de ficheros con la ruta exacta en el archivo archivos_peligrosos
 
-sudo ls -l -R / | grep -E '^.{7}rwx' >> archivos_peligrosos.txt
+ls -l -R . | grep -E "^-.{6,6}rwx" >> archivos_peligrosos.txt
--- t1_scripting/ej28.sh
+++ t1_scripting/ej28.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-
+averiguar la direccion de red
+averiguar la mascara de subred
+hacer ping a cada direccion de la red
+si está disponible, poner disponible
+si no lo está, poner no disponible
+enviar resultado a informe
