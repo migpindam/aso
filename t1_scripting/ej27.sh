a43a3acd133067bfc339fcdbbe87a2dafe2ba608
migpindam <migpindam@alu.edu.gva.es>
2024-09-30 08:55:38 +0200

    el 27 y cambio de nombres

--- t1_scripting/ej27.sh
+++ t1_scripting/ej27.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+#Crea un script que contenga una función que se le pase por parámetro el nombre de un fichero recibido como parámetro del script. Dicha función deberá verificar su existe el fichero o no. El nombre de la función es “existe”. Si el resultado es positivo, el script debe cambiar sus permisos a ejecutable para el propietario, pero no para el resto.
+
+function existe() {
+  [ -f "$1" ]
+}
+
+if existe=0; then
+  chmod u+x $1
+  #Permisos de ejecución del resto entiendo que se dejan como están, no se quitan.
+fi
+
 
