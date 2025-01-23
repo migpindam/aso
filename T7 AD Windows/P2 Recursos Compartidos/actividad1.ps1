89857e770c40cefc6a12d8136e49495850aff3b0
migpindam <m.pintor.d@gmail.com>
2025-01-23 08:46:31 +0100

    Add files via upload

---
+++ T7 AD Windows/P2 Recursos Compartidos/actividad1.ps1
@@ -0,0 +1,11 @@
+﻿New-Item C:\ASO -ItemType directory
+
+New-SmbShare -Path C:\ASO -Name aso
+
+Grant-SmbShareAccess -Name aso -AccountName Administrador -AccessRight Full
+
+Grant-SmbShareAccess -Name aso -AccountName profesores -AccessRight Full
+
+Grant-SmbShareAccess -Name aso -AccountName alumnos -AccessRight Read
+
+Revoke-SmbShareAccess -Name aso -AccountName Todos 
